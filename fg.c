623c44c590b4078b6f50bee7a379833e55a475fd
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-19 16:05:16 +0530

    Update fg.c

--- fg.c
+++ fg.c
@@ -2,27 +2,27 @@
 #include<string.h>
 int main(void)
 {
-	char a[10];
-	scanf("%s",a);
+char a[10];
+scanf("%s",a);
 
-	int i,flag=0,m;
-	m=strlen(a);
-	for(i=0;i<m;i++)
+int i,flag=0,m;
+m=strlen(a);
+for(i=0;i<m;i++)
+{
+if(a[i]=='1' || a[i]=='0')
+{
+		flag=1;
+	}
+	else
 	{
-		if(a[i]=='1' || a[i]=='0')
-		{
-			flag=1;
-		}
-		else
-		{
-			flag=0;
-			break;
-		}
+	flag=0;
+	break;
+}
 		
-	}
-	if(flag==1)
-	{
-		printf("yes");
-	}
+}
+if(flag==1)
+{
+printf("yes");
+}
 	
 }
