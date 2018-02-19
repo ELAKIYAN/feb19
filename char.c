2b2f670f851b488985d730c6ecfd75625d49902b
ELAKIYAN <31242650+ELAKIYAN@users.noreply.github.com>
2018-02-19 15:59:41 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	char k[1000];
+	int i;
+	printf("enter the character");
+	scanf("%c", k[1000]);
+	printf("enter the first k letter");
+	scanf("%d",&k);
+	for(i=o;i<=k;i++)
+	{
+		printf("%s", k[i]);
+}
+return 0;
+}
